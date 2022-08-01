70e30ce1ad81b1d0f6a358b465bb01adfce5fa01
贾奔奔 <jiabenben@yeah.net>
2022-08-01 20:58:13 +0800

    新增小程序答题查检表入参

---
+++ api/src/main/java/com/qu/modules/web/param/AnswerCheckAddParam.java
@@ -0,0 +1,18 @@
+package com.qu.modules.web.param;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel(value="答案入参", description="答案入参")
+public class AnswerCheckAddParam {
+    @ApiModelProperty(value = "总表id")
+    private Integer id;
+    @ApiModelProperty(value = "问卷id")
+    private Integer quId;
+    @ApiModelProperty(value = "状态 阶段性保存传0,提交传1")
+    private Integer status;
+    @ApiModelProperty(value = "答案数组")
+    private Answers[] answers;
+}
