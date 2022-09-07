591cdf427058706ec198a93151a6c167f9f88129
贾奔奔 <jiabenben@yeah.net>
2022-09-07 16:36:07 +0800

    修改问卷发布注释存id

--- api/src/main/java/com/qu/modules/web/service/impl/QuestionServiceImpl.java
+++ api/src/main/java/com/qu/modules/web/service/impl/QuestionServiceImpl.java
@@ -1,5 +1,21 @@
 package com.qu.modules.web.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -11,26 +27,44 @@ import com.qu.constant.QoptionConstant;
 import com.qu.constant.QsubjectConstant;
 import com.qu.constant.QuestionConstant;
 import com.qu.event.DeleteCheckDetailSetEvent;
-import com.qu.modules.web.entity.*;
-import com.qu.modules.web.mapper.*;
-import com.qu.modules.web.param.*;
+import com.qu.modules.web.entity.Qoption;
+import com.qu.modules.web.entity.Qsubject;
+import com.qu.modules.web.entity.Question;
+import com.qu.modules.web.entity.TbDep;
+import com.qu.modules.web.entity.TqmsQuotaCategory;
+import com.qu.modules.web.mapper.DynamicTableMapper;
+import com.qu.modules.web.mapper.OptionMapper;
+import com.qu.modules.web.mapper.QsubjectMapper;
+import com.qu.modules.web.mapper.QuestionMapper;
+import com.qu.modules.web.mapper.TqmsQuotaCategoryMapper;
+import com.qu.modules.web.param.QSingleDiseaseTakeStatisticAnalysisByDeptConditionParam;
+import com.qu.modules.web.param.QuestionAgainReleaseParam;
+import com.qu.modules.web.param.QuestionCheckParam;
+import com.qu.modules.web.param.QuestionEditParam;
+import com.qu.modules.web.param.QuestionParam;
+import com.qu.modules.web.param.UpdateCategoryIdParam;
+import com.qu.modules.web.param.UpdateDeptIdsParam;
+import com.qu.modules.web.param.UpdateQuestionIconParam;
+import com.qu.modules.web.param.UpdateWriteFrequencyIdsParam;
 import com.qu.modules.web.pojo.TbUser;
 import com.qu.modules.web.service.IQuestionService;
 import com.qu.modules.web.service.ITbDepService;
-import com.qu.modules.web.vo.*;
+import com.qu.modules.web.vo.QuestionAndCategoryPageVo;
+import com.qu.modules.web.vo.QuestionAndCategoryVo;
+import com.qu.modules.web.vo.QuestionCheckVo;
+import com.qu.modules.web.vo.QuestionMiniAppPageVo;
+import com.qu.modules.web.vo.QuestionMonthQuarterYearCreateListVo;
+import com.qu.modules.web.vo.QuestionPageVo;
+import com.qu.modules.web.vo.QuestionPatientCreateListVo;
+import com.qu.modules.web.vo.QuestionStatisticsCheckVo;
+import com.qu.modules.web.vo.QuestionVo;
+import com.qu.modules.web.vo.SubjectVo;
+import com.qu.modules.web.vo.ViewNameVo;
 import com.qu.util.DeptUtil;
 import com.qu.util.IntegerUtil;
 import com.qu.util.StringUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.util.*;
-import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Description: 问卷表
@@ -253,7 +287,7 @@ public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question> i
                             .append("(")
                             .append(limitWords)
                             .append(") COMMENT '")
-                            .append(qsubject.getSubName())
+                            .append(qsubject.getId())
                             .append("',");
 
                     if (QsubjectConstant.MARK_OPEN.equals(qsubject.getMark())) {
@@ -265,7 +299,7 @@ public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question> i
                                 .append("(")
                                 .append(QsubjectConstant.MARK_LENGTH)
                                 .append(") COMMENT '")
-                                .append(qsubject.getSubName())
+                                .append(qsubject.getId())
                                 .append("的痕迹")
                                 .append("',");
                         sql.append("`")
@@ -276,7 +310,7 @@ public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question> i
                                 .append("(")
                                 .append(QsubjectConstant.MARK_LENGTH)
                                 .append(") COMMENT '")
-                                .append(qsubject.getSubName())
+                                .append(qsubject.getId())
                                 .append("的痕迹图片")
                                 .append("',");
                     }
