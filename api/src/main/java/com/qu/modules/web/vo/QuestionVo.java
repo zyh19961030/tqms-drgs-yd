5d17794192c85620253a6133adfd92933cbac7eb
贾奔奔 <jiabenben@yeah.net>
2023-01-13 12:46:48 +0800

    查询问卷题目数据增加问卷分类反参

--- api/src/main/java/com/qu/modules/web/vo/QuestionVo.java
+++ api/src/main/java/com/qu/modules/web/vo/QuestionVo.java
@@ -1,72 +1,63 @@
 package com.qu.modules.web.vo;
 
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.List;
 
 @Data
 @ApiModel(value = "问卷Vo", description = "问卷Vo")
 public class QuestionVo {
-    @Excel(name = "主键", width = 15)
     @ApiModelProperty(value = "主键")
     private java.lang.Integer id;
     /**
      * 问卷名称
      */
-    @Excel(name = "问卷名称", width = 15)
     @ApiModelProperty(value = "问卷名称")
     private java.lang.String quName;
     /**
      * 问卷描述
      */
-    @Excel(name = "问卷描述", width = 15)
     @ApiModelProperty(value = "问卷描述")
     private java.lang.String quDesc;
     /**
      * 0:草稿箱 1:已发布
      */
-    @Excel(name = "0:草稿箱 1:已发布", width = 15)
     @ApiModelProperty(value = "0:草稿箱 1:已发布")
     private java.lang.Integer quStatus;
     /**
      * 0:正常1:已停用
      */
-    @Excel(name = "0:正常1:已停用", width = 15)
     @ApiModelProperty(value = "0:正常1:已停用")
     private java.lang.Integer quStop;
     /**
      * 科室查看权限，科室id逗号分割
      */
-    @Excel(name = "科室查看权限，科室id逗号分割", width = 15)
     @ApiModelProperty(value = "科室查看权限，科室id逗号分割")
     private java.lang.String deptIds;
     /**
      * 答案对应数据库名
      */
-    @Excel(name = "答案对应数据库名", width = 15)
     @ApiModelProperty(value = "答案对应数据库名")
     private java.lang.String tableName;
     /**
      * 0:正常1:已删除
      */
-    @Excel(name = "0:正常1:已删除", width = 15)
     @ApiModelProperty(value = "0:正常1:已删除")
     private java.lang.Integer del;
     /**
      * 创建者
      */
-    @Excel(name = "创建者", width = 15)
     @ApiModelProperty(value = "创建者")
     private java.lang.Integer creater;
     /**
      * 创建时间
      */
-    @Excel(name = "创建时间", width = 20, format = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建时间")
@@ -74,17 +65,23 @@ public class QuestionVo {
     /**
      * 更新者
      */
-    @Excel(name = "更新者", width = 15)
     @ApiModelProperty(value = "更新者")
     private java.lang.Integer updater;
     /**
      * 更新时间
      */
-    @Excel(name = "更新时间", width = 20, format = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新时间")
     private java.util.Date updateTime;
+
+    /**分类id*/
+    @ApiModelProperty(value = "分类id")
+    private java.lang.String categoryId;
+
+    /**0其他 1单病种 2检查表 3登记表*/
+    @ApiModelProperty(value = "0其他 1单病种 2检查表 3登记表")
+    private java.lang.Integer categoryType;
 
     @ApiModelProperty(value = "题目")
     private List<SubjectVo> subjectVoList;
