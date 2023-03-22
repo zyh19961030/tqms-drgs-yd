66cfe4c05585842a46c50977bb8f2ce323e885e5
贾奔奔 <jiabenben@yeah.net>
2023-03-22 14:32:00 +0800

    修改处理历史检查表统计明细数据已提交数据

--- api/src/main/java/com/qu/modules/web/service/impl/AdminPrivateServiceImpl.java
+++ api/src/main/java/com/qu/modules/web/service/impl/AdminPrivateServiceImpl.java
@@ -1,30 +1,16 @@
 package com.qu.modules.web.service.impl;
 
-import cn.hutool.core.date.*;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.google.common.collect.Lists;
-import com.qu.constant.*;
-import com.qu.event.AnswerCheckStatisticDetailEvent;
-import com.qu.modules.web.dto.AnswerCheckStatisticDetailEventDto;
-import com.qu.modules.web.entity.*;
-import com.qu.modules.web.mapper.*;
-import com.qu.modules.web.param.AdminPrivateParam;
-import com.qu.modules.web.param.AdminPrivateUpdateOptionValueParam;
-import com.qu.modules.web.param.AdminPrivateUpdateTableAddDelFeeParam;
-import com.qu.modules.web.param.AdminPrivateUpdateTableDrugFeeParam;
-import com.qu.modules.web.service.IAdminPrivateService;
-import com.qu.modules.web.service.IOptionService;
-import com.qu.modules.web.service.ISubjectService;
-import com.qu.modules.web.vo.SubjectVo;
-import com.qu.util.PriceUtil;
-import lombok.extern.slf4j.Slf4j;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.annotation.Resource;
+
 import org.apache.commons.lang3.StringUtils;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.api.vo.ResultFactory;
@@ -34,11 +20,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.math.BigDecimal;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.common.collect.Lists;
+import com.qu.constant.AnswerCheckConstant;
+import com.qu.constant.AnswerConstant;
+import com.qu.constant.QoptionConstant;
+import com.qu.constant.QsubjectConstant;
+import com.qu.constant.QuestionConstant;
+import com.qu.event.AnswerCheckStatisticDetailEvent;
+import com.qu.modules.web.dto.AnswerCheckStatisticDetailEventDto;
+import com.qu.modules.web.entity.Answer;
+import com.qu.modules.web.entity.AnswerCheck;
+import com.qu.modules.web.entity.QSingleDiseaseTake;
+import com.qu.modules.web.entity.Qoption;
+import com.qu.modules.web.entity.Qsubject;
+import com.qu.modules.web.entity.Question;
+import com.qu.modules.web.mapper.AnswerCheckMapper;
+import com.qu.modules.web.mapper.AnswerMapper;
+import com.qu.modules.web.mapper.DynamicTableMapper;
+import com.qu.modules.web.mapper.OptionMapper;
+import com.qu.modules.web.mapper.QSingleDiseaseTakeMapper;
+import com.qu.modules.web.mapper.QsubjectMapper;
+import com.qu.modules.web.mapper.QuestionMapper;
+import com.qu.modules.web.param.AdminPrivateParam;
+import com.qu.modules.web.param.AdminPrivateUpdateOptionValueParam;
+import com.qu.modules.web.param.AdminPrivateUpdateTableAddDelFeeParam;
+import com.qu.modules.web.param.AdminPrivateUpdateTableDrugFeeParam;
+import com.qu.modules.web.service.IAdminPrivateService;
+import com.qu.modules.web.service.IOptionService;
+import com.qu.modules.web.service.ISubjectService;
+import com.qu.modules.web.vo.SubjectVo;
+import com.qu.util.PriceUtil;
+
+import cn.hutool.core.date.DateException;
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
@@ -718,10 +744,13 @@ public class AdminPrivateServiceImpl extends ServiceImpl<AnswerMapper, Answer> i
         questionLambda.eq(Question::getDel, QuestionConstant.DEL_NORMAL);
         List<Question> questionList = questionMapper.selectList(questionLambda);
         Map<Integer, Question> questionMap = questionList.stream().collect(Collectors.toMap(Question::getId, Function.identity()));
+        StringBuffer res = new StringBuffer();
         for (AnswerCheck answerCheck : answerCheckList) {
             Question question = questionMap.get(answerCheck.getQuId());
             if (question == null || QuestionConstant.DEL_DELETED.equals(question.getDel())) {
-                return ResultFactory.error("问卷不存在,无法保存。");
+                res.append("问卷id->");
+                res.append(answerCheck.getQuId());
+                continue;
             }
 
             JSONArray answers = (JSONArray)JSONArray.parse(answerCheck.getAnswerJson());
@@ -748,6 +777,6 @@ public class AdminPrivateServiceImpl extends ServiceImpl<AnswerMapper, Answer> i
             applicationEventPublisher.publishEvent(questionVersionEvent);
         }
 
-        return ResultFactory.success();
+        return ResultFactory.success(res);
     }
 }
