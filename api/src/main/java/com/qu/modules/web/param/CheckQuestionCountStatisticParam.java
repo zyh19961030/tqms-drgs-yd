ce3329065e890c2edf00ad0c74f1b4ddd66565e1
贾奔奔 <jiabenben@yeah.net>
2023-04-07 15:44:51 +0800

    修改统计_查检数量统计增加填报时间筛选

--- api/src/main/java/com/qu/modules/web/param/CheckQuestionCountStatisticParam.java
+++ api/src/main/java/com/qu/modules/web/param/CheckQuestionCountStatisticParam.java
@@ -1,8 +1,13 @@
 package com.qu.modules.web.param;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.Pattern;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -22,6 +27,16 @@ public class CheckQuestionCountStatisticParam {
     @Pattern(regexp="\\d{4}-(0[1-9]|1[0-2])",message="选择检查月份_时间格式不对")
     @ApiModelProperty(value = "选择检查月份_时间  格式：月:yyyy-MM")
     private String checkMonth;
+
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @ApiModelProperty(value = "填报时间_起始时间 格式：yyyy-MM-dd")
+    private Date startDate;
+
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @ApiModelProperty(value = "填报时间_结束时间 格式：yyyy-MM-dd")
+    private Date endDate;
 
     @ApiModelProperty(value = "前端忽略")
     private List<String> idList;
