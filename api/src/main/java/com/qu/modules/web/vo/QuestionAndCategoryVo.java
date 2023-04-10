f21e167481786e7511c5b8337b504ea49e78e908
贾奔奔 <jiabenben@yeah.net>
2023-04-10 12:30:17 +0800

    修改开始检查权限，科主任、医生能看医疗和院感类型的，护士长、护士能看护理和院感类型的

--- api/src/main/java/com/qu/modules/web/vo/QuestionAndCategoryVo.java
+++ api/src/main/java/com/qu/modules/web/vo/QuestionAndCategoryVo.java
@@ -1,10 +1,12 @@
 package com.qu.modules.web.vo;
 
+import org.jeecgframework.poi.excel.annotation.Excel;
+
 import com.qu.modules.web.entity.Question;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import org.jeecgframework.poi.excel.annotation.Excel;
 
 @Data
 @ApiModel(value = "问卷分页Vo", description = "问卷分页Vo")
@@ -15,4 +17,8 @@ public class QuestionAndCategoryVo extends Question{
     @Excel(name = "分类名称", width = 15)
     @ApiModelProperty(value = "分类名称")
     private String categoryName;
+    /**关联检查项目名称*/
+    @Excel(name = "关联检查项目名称", width = 15)
+    @ApiModelProperty(value = "关联检查项目名称")
+    private java.lang.String checkProjectName;
 }
