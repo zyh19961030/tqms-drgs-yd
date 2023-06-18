f1617f7ac1cf485352ea2b63129a82284a7c2fcf
贾奔奔 <jiabenben@yeah.net>
2023-06-18 17:42:01 +0800

    修改录入表单查询处理中数据

--- api/src/main/java/com/qu/modules/web/service/impl/SingleEnterQuestionServiceImpl.java
+++ api/src/main/java/com/qu/modules/web/service/impl/SingleEnterQuestionServiceImpl.java
@@ -1,25 +1,7 @@
 package com.qu.modules.web.service.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import javax.annotation.Resource;
-
-import org.apache.commons.lang3.StringUtils;
-import org.jeecg.common.api.vo.ResultBetter;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -30,57 +12,29 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.qu.constant.AnswerConstant;
-import com.qu.constant.Constant;
-import com.qu.constant.QsubjectConstant;
-import com.qu.constant.QuestionConstant;
-import com.qu.constant.SingleEnterQuestionConstant;
-import com.qu.constant.TbDataConstant;
-import com.qu.modules.web.entity.Answer;
-import com.qu.modules.web.entity.Qsubject;
-import com.qu.modules.web.entity.Question;
-import com.qu.modules.web.entity.SingleEnterQuestion;
-import com.qu.modules.web.entity.SingleEnterQuestionColumn;
-import com.qu.modules.web.entity.SingleEnterQuestionSubject;
-import com.qu.modules.web.entity.TbData;
-import com.qu.modules.web.entity.TbDep;
+import com.qu.constant.*;
+import com.qu.modules.web.entity.*;
 import com.qu.modules.web.mapper.DynamicTableMapper;
 import com.qu.modules.web.mapper.SingleEnterQuestionMapper;
-import com.qu.modules.web.param.Answers;
-import com.qu.modules.web.param.IdIntegerParam;
-import com.qu.modules.web.param.QuestionCheckParam;
-import com.qu.modules.web.param.SingleEnterQuestionAddParam;
-import com.qu.modules.web.param.SingleEnterQuestionAmendmentSaveDataParam;
-import com.qu.modules.web.param.SingleEnterQuestionEnterQuestionHeadListParam;
-import com.qu.modules.web.param.SingleEnterQuestionEnterQuestionListParam;
-import com.qu.modules.web.param.SingleEnterQuestionListParam;
-import com.qu.modules.web.param.SingleEnterQuestionSaveDataParam;
-import com.qu.modules.web.param.SingleEnterQuestionUpdateParam;
+import com.qu.modules.web.param.*;
 import com.qu.modules.web.pojo.Data;
 import com.qu.modules.web.pojo.JsonRootBean;
-import com.qu.modules.web.service.IAnswerService;
-import com.qu.modules.web.service.IQuestionService;
-import com.qu.modules.web.service.ISingleEnterQuestionColumnService;
-import com.qu.modules.web.service.ISingleEnterQuestionService;
-import com.qu.modules.web.service.ISingleEnterQuestionSubjectService;
-import com.qu.modules.web.service.ISubjectService;
-import com.qu.modules.web.service.ITbDataService;
-import com.qu.modules.web.service.ITbDepService;
-import com.qu.modules.web.vo.AnswerIdVo;
-import com.qu.modules.web.vo.EnterQuestionDataListCompleteVo;
-import com.qu.modules.web.vo.SingleEnterQuestionEnterQuestionHeadListDetailVo;
-import com.qu.modules.web.vo.SingleEnterQuestionEnterQuestionHeadListVo;
-import com.qu.modules.web.vo.SingleEnterQuestionInfoSubjectVo;
-import com.qu.modules.web.vo.SingleEnterQuestionInfoVo;
-import com.qu.modules.web.vo.SingleEnterQuestionListVo;
-import com.qu.modules.web.vo.SingleEnterQuestionQuestionCheckVo;
-import com.qu.modules.web.vo.SubjectVo;
+import com.qu.modules.web.service.*;
+import com.qu.modules.web.vo.*;
 import com.qu.util.HttpClient;
 import com.qu.util.PojoUtils;
-
-import cn.hutool.core.collection.CollectionUtil;
-import cn.hutool.core.date.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.jeecg.common.api.vo.ResultBetter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @Description: 录入表单表
@@ -570,15 +524,15 @@ public class SingleEnterQuestionServiceImpl extends ServiceImpl<SingleEnterQuest
         sqlSelect.append(question.getTableName());
         sqlSelect.append("` where del = '0' and table_answer_status = '0'  ");
         if(param.getStatus().equals(SingleEnterQuestionConstant.ENTER_QUESTION_DATA_LIST_STATUS_HANDLE)){
-            sqlCount.append("and  ");
-            sqlCount.append(singleEnterQuestion.getSubjectColumnName());
-            sqlCount.append(" = 'y' ");
+            sqlSelect.append("and  ");
+            sqlSelect.append(singleEnterQuestion.getSubjectColumnName());
+            sqlSelect.append(" = 'y' ");
         }else{
-            sqlCount.append("and (");
-            sqlCount.append(singleEnterQuestion.getSubjectColumnName());
-            sqlCount.append(" = 'y1' or ");
-            sqlCount.append(singleEnterQuestion.getSubjectColumnName());
-            sqlCount.append(" = 'y2') ");
+            sqlSelect.append("and (");
+            sqlSelect.append(singleEnterQuestion.getSubjectColumnName());
+            sqlSelect.append(" = 'y1' or ");
+            sqlSelect.append(singleEnterQuestion.getSubjectColumnName());
+            sqlSelect.append(" = 'y2') ");
         }
         sqlSelect.append("limit ");
         sqlSelect.append((pageNo - 1) * pageSize);
